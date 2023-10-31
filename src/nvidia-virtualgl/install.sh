2cebbe9a3f305d6da7ff7be4dbdca23f891d8836
Masahiro Wada <argon.argon.argon@gmail.com>
2023-10-31 13:33:20 +0000

    add nvidia-virtualgl

---
+++ src/nvidia-virtualgl/install.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+TARGET_VIRTUALGL_VERSION="${VERSION:-"latest"}"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Figure out correct version of a three part version number is not passed
+find_version_from_git_tags() {
+    local variable_name=$1
+    local requested_version=${!variable_name}
+    if [ "${requested_version}" = "none" ]; then return; fi
+    local repository=$2
+    local prefix=${3:-"tags/v"}
+    local separator=${4:-"."}
+    local last_part_optional=${5:-"false"}    
+    if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
+        local escaped_separator=${separator//./\\.}
+        local last_part
+        if [ "${last_part_optional}" = "true" ]; then
+            last_part="(${escaped_separator}[0-9]+)?"
+        else
+            last_part="${escaped_separator}[0-9]+"
+        fi
+        local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
+        local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
+        if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
+            declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
+        else
+            set +e
+            declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
+            set -e
+        fi
+    fi
+    if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" > /dev/null 2>&1; then
+        echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
+        exit 1
+    fi
+    echo "${variable_name}=${!variable_name}"
+}
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        #apt-get -y install --no-install-recommends "$@"
+        apt-get -y install "$@"
+    fi
+}
+
+# Install mesa, git, other dependencies if missing
+check_packages libegl1-mesa libglu1-mesa  mesa-utils libxv1 libxtst6 x11-common
+if ! type git > /dev/null 2>&1; then
+    check_packages git
+fi
+
+# Get closest match for version number specified
+find_version_from_git_tags TARGET_VIRTUALGL_VERSION "https://github.com/VirtualGL/virtualgl" "tags/" "." "true"
+
+architecture="$(uname -m)"
+case $architecture in
+    x86_64) architecture="amd64";;
+    aarch64 | armv8*) architecture="arm64";;
+    aarch32 | armv7* | armvhf*) architecture="armv6l";;
+    i?86) architecture="386";;
+    *) echo "(!) Architecture $architecture unsupported"; exit 1 ;;
+esac
+
+# Install VirtualGL
+umask 0002
+echo "Downloading VirtualGL ${TARGET_VIRTUALGL_VERSION}..."
+curl -fsSL -o /tmp/virtualgl.deb "https://sourceforge.net/projects/virtualgl/files/${TARGET_VIRTUALGL_VERSION}/virtualgl_${TARGET_VIRTUALGL_VERSION}_${architecture}.deb"
+if [ "$?" != "0" ]; then
+    echo "(!) Download failed."
+    exit 1
+fi
+
+dpkg -i /tmp/virtualgl.deb
+
+# Configure VirtualGL
+/opt/VirtualGL/bin/vglserver_config +glx -s -f -t
+cat <<EOF >/usr/share/glvnd/egl_vendor.d/10_nvidia.json
+{
+    "file_format_version" : "1.0.0",
+    "ICD" : {
+        "library_path" : "libEGL_nvidia.so.0"
+    }
+}
+EOF
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
